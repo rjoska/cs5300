22d4b0de3740f9d4a89e4fa21add519ac18547b0
Roman Joska <rjoska@uccs.edu>
2026-03-01 17:17:36 +0000

    Added render.yaml

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
